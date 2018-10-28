ca7a41b5c58190164ce76ba7174f154c0c70d49b
Ofir Smuha <ofir@500tech.com>
2018-10-28 08:03:21 +0200

    interface setup

--- proj/src/App.js
+++ proj/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Trends from './components/trends/Trends'
+import Interface from './components/interface/Interface'
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Trends/>
+        <Interface/>
       </div>
     );
   }
